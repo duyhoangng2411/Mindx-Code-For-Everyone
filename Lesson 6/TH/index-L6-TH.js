f47c8f9e2b4b052c8df3076801b14235c4b85153
duyhoangng2411 <duyhoangng.2411@gmail.com>
2026-07-08 13:20:44 +0700

    commit

---
+++ Lesson 6/TH/index-L6-TH.js
@@ -0,0 +1,72 @@
+// 1. Tính diện tích và chu vi hình chữ nhật 
+let length = 1;
+let width = 2;
+const area = length*width
+const perimeter = (length+width)*2
+console.log(`Diện tích là: ${area} m2`)
+console.log(`Chu vi là: ${perimeter} m`)
+
+// 2.Viết 1 chương trình JS để chuyển
+// đổi 5 (giây) nhập từ người dùng thành
+// // dạng giờ phút giây
+
+const seconds = 12345
+const hours = (seconds - seconds % 3600) / 3600
+const minutes = ((seconds % 3600) - (seconds % 3600) % 60) / 60;
+const second = seconds - minutes * 60 - hours * 3600;
+console.log(`${hours}h ${minutes}m ${second}s`)
+
+// 3. Viết một chương trình JS để tính lũy
+// // thừa của một số nhập từ người dùng.
+
+const luyThua = 2
+const coSo = 5
+// c1 
+const ketQua = Math.pow(coSo, luyThua)
+console.log("🚀 ~ ketQua:", ketQua)
+// c2 
+const pow = coSo**luyThua
+console.log("🚀 ~ pow:", pow)
+
+// Math.sqrt => căn bậc 2
+
+// 4. Tính tổng trung bình cộng của 3 số bất kỳ 
+const number1 = 9;
+const number2 = 8;
+const number3 = 7;
+const avg = (number1 + number2 + number3) / 3
+console.log("🚀 ~ Trung bình cộng là:", avg)
+
+// 5. Viết 1 ct JS để tính
+// khoảng cách giữa 2 điểm trong không gian 20.
+
+const x1 = 2;
+const y1 = 3;
+const x2 = 5;
+const y2 = 7;
+
+const distance = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2))
+console.log("🚀 ~ distance:", distance)
+
+// 6. Viết 1 ct JS để ss 3 số nhập từ người dùng và trả về
+// true nếu số lớn nhất là số thứ nhât, ngược lại trở về
+// false
+
+// c1 
+const isMax = (number1 > number2) && (number1 > number3);
+console.log("🚀 ~ isMax:", isMax)
+
+// c2 
+// const isMaxnotVer = (number1 <= number2) || (number1 <= number3)
+// console.log("🚀 ~ isMaxnotVer:", isMaxnotVer)
+
+// 7.Viết 1 ct JS để so sánh hai chuỗi nhập từ
+// người dùng và trả về true nếu chương trình
+// giống nhau, ngược lại trả về false.
+
+const str = "Tôi là Duy"
+const compareStr = "Tôi là Duy"
+// Toán tử 3 ngôi (L7) 
+const compareResult = str == compareStr ? console.log("Bằng nhau") : console.log("Không bằng nhau");
+
+
---
+++ Lesson 7/TH/index-L7-TH.js
@@ -0,0 +1,74 @@
+// 1. Sử dụng vòng lặp, tính tổng các chữ số từ 1 đến 100
+// 1+2+3+4+..+100
+// let sum = 0;
+// for(let i = 1; i <= 100; i++){
+//     sum +=1 // sum = sum + 1
+//     // console.log(`🚀 sum = ${sum}, i = ${i}:`)
+// }
+
+// // 2. Các số từ 0 đến 100, in ra các số chia hết cho 3 và 5. 
+
+// // c1 
+// for (let index = 0; index < 100; index++) {
+//     if(index % 5 === 0 && index % 3 === 0){
+//         console.log(`Số chia hết cho 5 và 3 là`, index)
+//     }
+// }
+
+// // c2
+// for (let index = 0; index < 100; index++) {
+//     if(index % (3*5) === 0){
+//         console.log(`Số chia hết cho 5 và 3 là`, index)
+//     }
+// }
+
+// // c3
+// for (let index = 0; index < 100; index += 15) {
+//         console.log(`Số chia hết cho 5 và 3 là`, index)
+// }
+
+// 3.Tính giai thừa của một số nguyên bất kỳ.
+// let giaiThua = 1;
+// const soNguyen = 10;
+// for (let item = 1; item <= soNguyen; item++) {
+//     console.log(` giaiThua = ${giaiThua} * ${item} = ${giaiThua*item}`)
+//     giaiThua = giaiThua * item;
+// }
+// console.log("🚀 ~ giaiThua:", giaiThua)
+
+// 4. Sử dụng vòng lặp for để in ra bảng cửu chương từ 2 đén 9.
+// for(let i = 1; i <= 9 ; i++){
+//         //Các bảng nhân
+//     console.log(`Bảng nhân ${i}`);
+//     for(let j = 1; j <= 10; j++) {
+//         //Các phép tính
+//         console.log(`${i} x ${j} = ${i * j}`);
+//     }
+// }
+
+// 5. Hãy chuyển một số nguyên dương sang dạng
+// nhị phân (gồm các chữ số 0 và 1).
+
+// 35/2=17 (1)
+// 17/2=8 (1)
+// 8/2=4 (0)
+// 4/2=2 (0)
+// 2/2=1 (0)
+// 1/2=0 (1)
+
+// let soNguyen = 35;
+// let nhiPhan = ""
+// while(soNguyen > 0){
+//     let soDu = soNguyen % 2
+//     nhiPhan+=soDu
+//     soNguyen = Math.floor(soNguyen / 2)
+// }
+// console.log("🚀 ~ nhiPhan:", nhiPhan.split("").reverse().join(``))
+
+// 6. Sử dụng vòng lặp, thực hiện tính phép tính sau với n là số nguyên:
+const n = 5;
+let sqrt = 0
+for(let i = 1; i <= n; i++){
+    sqrt = Math.sqrt(i + sqrt)
+}
+
